016cf823c5cca7bb7d020482ab32b392086bc794
Rylie Nabors <RylieNabors@my.unt.edu>
2025-04-02 03:52:59 -0500

    Fixed height issue and centered logo

--- mywebsite/core/static/javascript/logic.js
+++ mywebsite/core/static/javascript/logic.js
@@ -25,9 +25,16 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+// set main height here
+let logo = document.getElementsByClassName("logo-container")[0];
+const logoHeight = logo.offsetHeight;
+let mainHeight = main.offsetHeight;
+let newMainHeight = mainHeight - logoHeight;
+main.style.setProperty('--box-height', newMainHeight + 'px');
+
+
 let settingsRequested = true; // track settings popup state
 document.getElementById('settings-btn').addEventListener('click', function() {
-    //alert("Settings clicked! Implement your settings menu here.");
     let settingsPopup = document.getElementsByClassName("settings-popup")[0];
 
     if (settingsRequested) {
