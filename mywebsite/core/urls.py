22711478891fc7106539e5d98c9933a6211f5f03
jollyt03 <jollyt03@gmail.com>
2025-10-21 10:17:02 -0500

    Update urls.py for "startBackend & endBackend" func

--- mywebsite/core/urls.py
+++ mywebsite/core/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from .views import chat_page, chatbot_reply
 from . import views
-
+from .views import start_backend, end_backend
 
 urlpatterns = [
     path('', chat_page, name='chat_page'),
     path('chatbot/', views.chatbot_reply, name='chatbot'),
     path('get-verse-references/', views.GetVerseReferences, name='get_verse_references'),
+]
+
+urlpatterns = [
+    path("api/start_backend/", start_backend, name="start_backend"),
+    path("api/end_backend/", end_backend, name="end_backend"),
 ]
